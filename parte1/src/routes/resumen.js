9c08534e35c34a84e839b98767104f2230b91b0b
Jhon Alex Gaviria Tobon <j.gaviria@utp.edu.co>
2021-08-05 19:16:15 -0500

    fix: bug traversing the dates

--- parte1/src/routes/resumen.js
+++ parte1/src/routes/resumen.js
@@ -4,43 +4,44 @@ const fetch = require('node-fetch')
 const app = Router()
 
 
-function creatJson (total, comprasPorTDC, nocompraron, compraMasAlta){
+function creatJson (total, ansPorTDC, nocompraron, compraMasAlta){
     newJson = {}
     newJson["total"] = total
-    newJson["comprasPorTDC"] = comprasPorTDC
+    newJson["ansPorTDC"] = ansPorTDC
     newJson["nocompraron"] = nocompraron
     newJson["CompraMasAlta"] = compraMasAlta
     return newJson
 }
 
-async function calcular (ans, firstDay, lastDay, numDays){
-    let compras = await ans.json()
-    let n = compras.length
+async function calcular (ans){
+
     let noCompro = 0; 
     let total = 0; 
     let compraMasAlta = 0;
     let mapTDC = {}
-
-    for(let i=0; i<n; i++){
-        let thisDay = new Date (compras[i].date)
-        if(thisDay >= firstDay && thisDay <= lastDay){
-            if(!compras[i].compro){
+    
+    for(let k = 0; k<ans.length; k++){
+        for(let i=0; i<ans[k].length; i++){
+            
+            if(!ans[k][i].compro){
                 noCompro++
             } else {
-                if(compras[i].monto > compraMasAlta){
-                    compraMasAlta = compras[i].monto
+                if(ans[k][i].monto > compraMasAlta){
+                    compraMasAlta = ans[k][i].monto
                 }
-                let monto = parseInt(compras[i].monto)
-                total = monto
-                if (mapTDC[compras[i].tdc] == undefined){
-                    mapTDC[compras[i].tdc] = monto
+                let monto = parseInt(ans[k][i].monto) 
+                total += monto
+                if (mapTDC[ans[k][i].tdc] == undefined){
+                    mapTDC[ans[k][i].tdc] = monto
                 } else {
-                    mapTDC[compras[i].tdc] += monto
+                    mapTDC[ans[k][i].tdc] += monto
                 }           
             }
+
         }
     }
     let newJson = creatJson(total, mapTDC, noCompro, compraMasAlta)
+    // console.log(newJson)
     return newJson
 }  
  
@@ -48,12 +49,23 @@ app.get('/resumen/:day', async (req,res) =>{
     let firstDay = new Date(req.params.day)
     let lastDay = new Date(req.params.day)
     let numDays = parseInt(req.query.dias, 10) 
-
-    const ans = await fetch('https://apirecruit-gjvkhl2c6a-uc.a.run.app/compras/2019-12-01')
     lastDay.setDate(lastDay.getDate() + numDays) 
+    firstDay.setDate(firstDay.getDate() + 1) 
+    let ans = [] 
+    for(let i = firstDay; i<=lastDay; i.setDate(i.getDate() + 1)){
+        let year = i.getFullYear() 
+        let month = i.getMonth() + 1
+        month = month < 9 ? "0" + month.toString() : month
+        let thisday = i.getDate()
+        thisday = thisday < 9 ? "0" + thisday.toString() : thisday
+        console.log(`${year}-${month}-${thisday}`)
 
-    var newJson = await calcular(ans, firstDay, lastDay, numDays)
-    res.json(newJson)
+        let queri =  await fetch(`https://apirecruit-gjvkhl2c6a-uc.a.run.app/compras/${year}-${month}-${thisday}`)
+        ans.push(await queri.json())
+    }
+    
+    var newJson = await calcular(ans)
+    res.json(newJson) 
 })     
 
 module.exports = app  
